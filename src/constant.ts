a8fc27ee3b6c0e4dfad7a83ca33c49a6f7d066d5
ivanjermakov <ivanjermakov1@gmail.com>
2026-03-22 23:35:00 +0100

    Correct dt

--- src/constant.ts
+++ src/constant.ts
@@ -2,5 +2,5 @@ import { Vector3 } from 'three'
 
 export const gravity = new Vector3(0, -9.8, 0)
 export const fps = 60
-export const substeps = 2
-export const dt = 1 / (substeps * fps)
+export const substeps = 1
+export const dt = 1 / fps
